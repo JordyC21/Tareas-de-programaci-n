4015134a14210152b6ae84533dcbc38ee351a4ba
JordyC21 <87671558+JordyC21@users.noreply.github.com>
2022-02-21 14:51:21 -0600

    update

---
+++ Ejercicio de cola/Ejercicio de cola/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+public class EjemploQueue
+{
+
+    public static void Main()
+    {
+
+        Queue miQ = new Queue();
+        miQ.Enqueue("Uno");
+        miQ.Enqueue("Dos");
+        miQ.Enqueue("Tres");
+        miQ.Enqueue("Cuatro");
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("\nElementos guardados en la cola de nombre miQ");
+        Console.ResetColor();
+
+        Console.WriteLine(" ");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("\nConteo de la cantidad de elementos en la cola {0}");
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(miQ.Count);
+        Console.ResetColor();
+
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write("\nValores dentro de la cola:");
+        Console.ResetColor();
+
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        PrintValues(miQ);
+        Console.ResetColor();
+
+
+        Console.WriteLine(" ");
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Removiendo un número de la cola (Dequeue){0}");
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Red; 
+        Console.WriteLine("{0}", miQ.Dequeue());
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write("\nMostrando valores de Queue:");
+        Console.ResetColor();
+
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        PrintValues(miQ);
+        Console.ResetColor();
+
+        Console.WriteLine("");
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Removiendo otro número  (Dequeue)");
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(" {0}", miQ.Dequeue());
+        Console.ResetColor();
+
+        Console.WriteLine("");
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write("\nMostrando valores de la Queue:");
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        PrintValues(miQ);
+        Console.ResetColor();
+
+        Console.WriteLine("");
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Mostrando primer elemento sin removerlos (Peek)");
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("{0}", miQ.Peek());
+        Console.ResetColor();
+
+        Console.WriteLine("");
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write("\nMostrando los valores de la Queue:");
+        Console.ResetColor();
+
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        PrintValues(miQ);
+        Console.ResetColor();
+
+        Console.WriteLine("");
+
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("\nConteo de los valores: {0}");
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(miQ.Count);
+        Console.ResetColor();
+
+        Console.WriteLine("");
+
+        //Commentare este por que no lo pedia en la tarea.
+        //miQ.Clear();
+
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write("\nValores que quedaron en la cola:");
+        Console.ForegroundColor = ConsoleColor.Red;
+        PrintValues(miQ);
+        Console.ResetColor();
+    }
+
+    public static void PrintValues(IEnumerable myCollection)
+    {
+        foreach (Object obj in myCollection)
+            Console.Write("\t {0}", obj);
+        Console.WriteLine();
+    }
+
+
+}
---
+++ Ejercicio de pilas/Ejercicio de pilas/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Ejemplo
+{
+    public static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(" Admins ");
+        Console.ResetColor();
+        Stack<string> personas = new Stack<string>();
+        Console.ForegroundColor = ConsoleColor.Red;
+        personas.Push(" Jordy Enrique Cortez Aguilar");
+        personas.Push(" Ariel Ediberto Castro ");
+        personas.Push(" Wendy Liseth Chacon Pineda");
+        personas.Push(" Marvin Johalmo Rivas Ramirez");
+        personas.Push(" Victor Ernesto Navarro Monge");
+
+
+        foreach (string person in personas)
+        {
+            Console.WriteLine(person);
+        }
+        Console.ResetColor();
+
+        Console.WriteLine("\nPeek/El primer nombre a desapilar será : {0}",
+            personas.Peek());
+        Console.WriteLine("Popping/ Eliminando '{0}'", personas.Pop());
+        Console.WriteLine("\nPeek/viendo en el siguiente item a desapilar: {0}",
+            personas.Peek());
+        Console.WriteLine("Popping/ Eliminando '{0}'", personas.Pop());
+
+        Stack<string> Personas2 = new Stack<string>(personas.ToArray());
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("\nContenido de la primer copia:");
+        Console.ResetColor();
+        foreach (string person in Personas2)
+        {
+            Console.WriteLine(person);
+        }
+
+        //------------------------------------------------
+
+        string[] array2 = new string[personas.Count * 2];
+        personas.CopyTo(array2, personas.Count);
+        Stack<string> Personas3 = new Stack<string>(array2);
+
+        Console.WriteLine("\nContenido de la segunda copia, con duplicados y nulos:");
+        foreach (string person in Personas3)
+        {
+            Console.WriteLine(person);
+        }
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("\nVerificando si la pila # 2 contiene a la persona Victor Ernesto Navarro Monge");
+        Console.ResetColor();
+        Console.WriteLine("\nLa pila #2 contiene \"Victor Ernesto Navarro Monge\" = {0}",
+        Personas2.Contains("Victor Ernesto Navarro Monge"));
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("\nLimpiando la pila #2");
+        Console.ResetColor();
+
+        Console.WriteLine("\nPersonas2.Clear()");
+        Personas2.Clear();
+        Console.WriteLine("\nPersonas2.Count = {0}", Personas2.Count);
+
+    }
+}
+
+    
+
+
